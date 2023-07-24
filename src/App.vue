d8b184661671ae7c0f80199797c8fc8051b99c76
Weltra <2124954906@qq.com>
2023-07-24 09:12:15 +0800

    添加页脚控制台

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <SmartCity />
   <MapControls />
   <G2Charts />
+  <Footer />
 </template>
 
 <script setup>
@@ -11,6 +12,7 @@ import SmartCity from './views/SmartCity/index.vue'
 import MapControls from './components/MapControls.vue'
 import Header from './components/Header.vue'
 import G2Charts from './components/G2Charts/index.vue'
+import Footer from './components/Footer/index.vue'
 </script>
 
 <style>
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script setup></script>
+
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 80px;
+  z-index: 90;
+  background: url('../../assets/images/footer.png') center no-repeat;
+  background-size: cover;
+  transform: rotate(180deg);
+}
+</style>
