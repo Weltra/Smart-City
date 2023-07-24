7e7f06e6e0219a4f3f15934bb719b8f5d606730c
Weltra <2124954906@qq.com>
2023-07-24 15:45:20 +0800

    修改画框查询组件

--- src/components/Footer/DrawTools.vue
+++ src/components/Footer/DrawTools.vue
@@ -1,19 +1,17 @@
 <template>
-  <!-- 我们使用el-popover组件，将BottomTool中的内容插入进来 -->
   <el-popover
     placement="top"
-    :width="100"
     trigger="click"
-    popper-style="background-color: #53697670;color:#fff"
+    popper-style="background-color:#53697670;color:#fff"
+    :width="100"
   >
     <template #reference>
       <slot></slot>
     </template>
-    <!-- 这里是popover中的内容 -->
     <div class="popover-w">
       <i
         v-for="item in tools"
-        :class="computeClass(item)"
+        :class="computedClass(item)"
         @click="queryEvents(item)"
       ></i>
     </div>
@@ -21,24 +19,34 @@
 </template>
 
 <script setup>
-import { computed, inject, ref } from 'vue'
+import { computed, inject, onMounted, ref } from 'vue'
 import { DrawEvent, DrawPolygon, DrawCircle, DrawRect } from '@antv/l7-draw'
-
-// 绘制项，我们使用这个数组进行循环
+import { point, polygon, booleanPointInPolygon } from '@turf/turf'
+import { getEvents } from '@/api/smart_city.js'
+// 定义普通数据
+let eventsData = null
+// 定义tools
 const tools = ref([
   'drawPolygonTool',
   'drawRectTool',
   'drawCircleTool',
   'delete',
 ])
-// 每一项的样式，这里使用计算属性，计算每一项应该使用哪个icon
-const computeClass = computed(() => (item) => {
-  const res = {
-    iconfont: true,
-    'query-item': true,
+// 定义计算属性
+const computedClass = computed(() => {
+  return (item) => {
+    const res = {
+      iconfont: true,
+      'query-item': true,
+    }
+    res[`icon-${item}`] = true
+    return res
   }
-  res[`icon-${item}`] = true
-  return res
+})
+// 在onMounted中获取事故数据
+onMounted(async () => {
+  const res = await getEvents()
+  eventsData = res.features
 })
 
 // 定义查询绘制函数
@@ -65,11 +73,32 @@ function queryEvents(type) {
   }
   draw.enable()
   draw.on(DrawEvent.Change, (allFeatures) => {
+    // 找出最后绘制的图形(多边形)
+    const activeFeature = allFeatures[allFeatures.length - 1]
+    // 只保留最后绘制的图形
     allFeatures.forEach((item, index) => {
       if (index !== allFeatures.length - 1) {
         draw.removeFeature(item)
       }
     })
+
+    if (eventsData.length && activeFeature) {
+      const {
+        geometry: { coordinates: coordinatesActive },
+      } = activeFeature
+
+      // 使用turf判断哪些事故点落在绘制的拉框中
+      const resData = eventsData.filter((item) => {
+        const { geometry } = item
+        if (geometry.type === 'Point') {
+          const pt = point(geometry.coordinates)
+          const poly = polygon(coordinatesActive)
+          const isInArea = booleanPointInPolygon(pt, poly)
+          return isInArea
+        }
+      })
+      console.log(resData)
+    }
   })
 }
 </script>
