1a2d72eeaa3338b5b9353f1e70728c48ddef59d2
Weltra <2124954906@qq.com>
2023-07-26 20:32:12 +0800

    更改数据源

--- src/views/SmartCity/hooks/useHeatData.js
+++ src/views/SmartCity/hooks/useHeatData.js
@@ -1,12 +1,12 @@
 // 导入接口
-import { getHangzhouEvent } from '@/api/smart_city.js'
+import { getEvents } from '@/api/smart_city.js'
 // 官方文档设置三维高度，存在表面效应
 // 拟在event事件中添加times属性记录出现车祸的次数，用于渲染
 import { HeatmapLayer } from '@antv/l7'
 
 export default async (type, colors) => {
   // 获取数据
-  const heatData = await getHangzhouEvent()
+  const heatData = await getEvents()
   const heatMap = new HeatmapLayer({
     name: 'heatmap',
   })
